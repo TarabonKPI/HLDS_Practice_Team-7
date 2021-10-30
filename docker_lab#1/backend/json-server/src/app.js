dd92bcb5e457d1072aeff49c70a65e4e839caadb
TarabonKPI <91243431+TarabonKPI@users.noreply.github.com>
2021-10-30 10:28:07 +0300

    Add files via upload

---
+++ docker_lab#1/backend/json-server/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 4001;
+const dbPort = process.env.DB_PORT;
+const dbHost = process.env.DB_HOST;
+
+const app = express();
+app.set("json spaces", 2);
+
+app.get("/team_members", async (req, res) => {
+    try {
+        const resp = await axios.get(`http://${dbHost}:${dbPort}/team_members`);
+        res.json(resp.data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
